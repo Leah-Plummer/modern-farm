3474161737935105f6637f5ecde7d8f78b7b867e
Leah Plummer <leahplumm@gmail.com>
2022-09-15 13:46:07 -0500

    added catalog module

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,12 +1,13 @@
+let harvestedPlants=[]
 export const harvestPlants = (plantsToBeHarvested) => {
     for (let plant of plantsToBeHarvested) {
-       let howManyToHarvest = plant.output
-       if (plant.type == "Corn") {
-        howManyToHarvest/2
-       }
-       for (let i = 0; i < howManyToHarvest; i++) {
-        plantsToBeHarvested.push(plant)
-       }
+        let howManyToHarvest = plant.output
+       if (plant.type === "Corn") {
+        howManyToHarvest=howManyToHarvest/2
     }
-    return plantsToBeHarvested
+    for (let i = 0; i < howManyToHarvest; i++) {
+     harvestedPlants.push(plant)
+    }
+    }
+    return harvestedPlants
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ import { createPlan } from "./plan.js"
 import { addPlant, usePlants } from "./field.js"
 import { plantSeeds } from "./tractor.js"
 import { harvestPlants } from "./harvester.js"
+import { catalog } from "./catalog.js"
 
 const testArray = [
     {
@@ -11,7 +12,7 @@ const testArray = [
         height: 24,
         output: 4
     }, 
-        [
+        
     {
     type: "Corn",
     height: 180,
@@ -22,7 +23,7 @@ const testArray = [
         height: 180,
         output: 6   
     },
-        ],
+        
        
 ] 
 
@@ -40,3 +41,6 @@ console.log(secondTest)
 
 const thirdTest = harvestPlants(secondTest)
 console.log(thirdTest)
+
+const parentHTMLElement = document.querySelector(".container")
+parentHTMLElement.innerHTML = catalog(thirdTest)
